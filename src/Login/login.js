dfbf3d178a8588efbfd07985096b3b4e05f868ef
DiegoMont2002 <mdiegoirvin010@gmail.com>
2025-04-23 23:06:04 -0600

    Modificacion de para un mejor flujo

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import api from '../Services/api';
 import { FiLogIn, FiUserPlus, FiMail, FiLock } from 'react-icons/fi';
 import './login.css';
 import Carousel from './carousel';
